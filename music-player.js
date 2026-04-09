ebf28e4c47f6947af83203cf8b7558aec3187da0
redp1337 <mparedes754@gmail.com>
2026-04-09 14:44:16 -0400

    add play, stop, next, back button functionality

--- music-player.js
+++ music-player.js
@@ -9,11 +9,36 @@ let song_index=0;
 let isCurrentlyPlaying =false;
 
 let playlist = [
-    {name: "song1", file: "./music/song1.mp3"},
-    {name: "song2", file: "./music/song2.mp3"},
-    {name: "song3", file: "./music/song3.mp3"},
+    {name: "Track 1 : trees", file: "./music/song1.mp3"},
+    {name: "Track 2 : maple", file: "./music/song2.mp3"},
+    {name: "Track 3 : apple", file: "./music/song3.mp3"},
 ]
 
 let songAudio = new Audio(playlist[song_index].file);
 
+currently_playing.textContent=playlist[song_index].name;
 
+play_button.addEventListener("click", function(){
+    songAudio.src=playlist[song_index].file;
+    songAudio.play();
+    currently_playing.textContent= playlist[song_index].name;
+});
+
+stop_button.addEventListener("click", function(){
+    songAudio.pause();
+});
+
+next_button.addEventListener("click", function(){
+    song_index=(song_index+1)%playlist.length;
+    songAudio.src=playlist[song_index].file;
+    songAudio.play();
+    songAudio.currentTime=0;
+    currently_playing.textContent= playlist[song_index].name;
+})
+
+back_button.addEventListener("click", function(){
+    song_index=(song_index-1)%playlist.length;
+    songAudio.src=playlist[song_index].file;
+    songAudio.play();
+    currently_playing.textContent= playlist[song_index].name;
+})
