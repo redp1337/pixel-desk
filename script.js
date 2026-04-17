1365c95fae00bd205afcb856aabb5615951104b8
sleepypops21 <keegan_22@outlook.com>
2026-04-17 16:38:20 -0400

    Add sound code within the enterPass section

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ const sounds = {
     close: new Audio("sounds/close.mp3") 
     save: new Audio("sounds/save.mp3")
     play: new Audio("sounds/play.mp3") 
-    pause: new Audio("sounds/pause.mp3") 
+    pause: new Audio("sounds/pause.mp3")
 }; 
+
+function playSound(sound) { 
+    sound.currentTime = 0; 
+    sound.play(); 
+} 
 
 function enterPass(){
     const userPassEntry= document.getElementById('pass').value;
     if (correctPass=== userPassEntry){
+        playSound(sounds.click);
         window.location.href='desktop.html';
-    } else{
+    } else{ 
+        playSound(sounds.click);
         document.getElementById('error-box').style.display="flex";
     }
 }
