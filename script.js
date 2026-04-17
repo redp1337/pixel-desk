1f60a43f0c952d4744c3c85566f8c13e3781f6de
sleepypops21 <keegan_22@outlook.com>
2026-04-17 16:30:45 -0400

    Added code for sounds

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const correctPass="36";
- 
+
+const sounds = { 
+    click: new Audio("sounds/click.mp3") 
+    close: new Audio("sounds/close.mp3") 
+    save: new Audio("sounds/save.mp3")
+    play: new Audio("sounds/play.mp3") 
+    pause: new Audio("sounds/pause.mp3") 
+}; 
+
 function enterPass(){
     const userPassEntry= document.getElementById('pass').value;
     if (correctPass=== userPassEntry){
@@ -8,10 +16,8 @@ function enterPass(){
         document.getElementById('error-box').style.display="flex";
     }
 }
- 
- 
+
 function closeError(){
     document.getElementById('error-box').style.display='none';
 }
 
- 
